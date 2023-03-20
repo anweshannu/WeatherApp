b27095307e5a2db79b5d03229c514726cb9e7d3d
Anwesh Mandaloji <anweshannu111@gmail.com>
2023-03-20 15:34:32 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Anwesh on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
